196cca590cdac0061ebbdb17e6b6f989e793893d
keoni burns <keoni.burns@gmail.com>
2025-03-28 00:52:36 +0000

    added some deployment shiz

---
+++ terraform/main.tf
@@ -0,0 +1,110 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 bucket for website hosting
+resource "aws_s3_bucket" "website" {
+  bucket = "your-portfolio-bucket-name"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# Bucket policy to allow public read
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      },
+    ]
+  })
+}
+
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "website" {
+  enabled             = true
+  is_ipv6_enabled    = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.website.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3Origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "website" {
+  comment = "OAI for portfolio website"
+}
+
+# Outputs
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.website.domain_name
+}
+
+output "s3_bucket" {
+  value = aws_s3_bucket.website.id
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.website.id
+} 
